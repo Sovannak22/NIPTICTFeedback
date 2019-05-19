19c31b5e99d2e807f9a475dcabeed2a4e7eb0d8f
kimsan <saikimsan205gmail.com>
2019-05-19 20:36:05 +0700

    Merge branch 'Vannak' of https://github.com/Sovannak22/NIPTICTFeedback into Kimsan
    # Conflicts:
    #	app/build.gradle

--- app/src/main/java/com/example/niptictfeedback/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/niptictfeedback/fragments/ProfileFragment.java
@@ -3,21 +3,62 @@ package com.example.niptictfeedback.fragments;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabItem;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.niptictfeedback.R;
+import com.example.niptictfeedback.adapter.page_adapter.PageAdapter;
+import com.example.niptictfeedback.adapter.page_adapter.ProfilePageAdapter;
 
 public class ProfileFragment extends Fragment {
     public ProfileFragment() {
     }
 
+    TabLayout tabLayout;
+    TabItem publicTabItem,privateTabItem,responseTabItem;
+    ViewPager viewPager;
+    ProfilePageAdapter adapter;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_profile,container,false);
+
+
+        tabLayout = v.findViewById(R.id.tabLayout_id);
+        publicTabItem = v.findViewById(R.id.tab_public);
+        privateTabItem = v.findViewById(R.id.tab_private);
+        responseTabItem= v.findViewById(R.id.tab_response);
+        viewPager = v.findViewById(R.id.viewPager_id);
+
+
+        adapter = new ProfilePageAdapter(getActivity(), getChildFragmentManager(),tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
         return v;
     }
 }
